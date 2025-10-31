944949dd0283b9c3e6f6b4fcce13f0dc2cfaab14
HADIL19 <hadylkhelif18@gmail.com>
2025-10-31 09:11:08 +0100

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config(); // MUST be first
+const app = require('./app');
+const { PORT = 4000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
